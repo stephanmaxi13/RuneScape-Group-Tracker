6c741bcde45a2310289a95c6487121350218d0d7
Stephan Maxi <stephan.maxi13@gmail.com>
2026-01-19 04:08:35 -0500

    Restructured Folder Structure to allign with best practices and overall learning

--- src/players/players.service.spec.ts
+++ src/players/players.service.spec.ts
@@ -1,24 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
 import { HttpService } from '@nestjs/axios';
-import { Player } from './players/schemas/player.schema';
-import { Gains } from './groups/schemas/gains.schema';
-import { PlayersService } from './players/players.service';
+import { PlayersService } from './players.service';
+import { Player } from './schemas/player.schema';
+import { Gains } from '../groups/schemas/gains.schema';
 
 describe('PlayersService', () => {
   let service: PlayersService;
 
-  // 1. Create a mock for the HttpService
+  // Mock for HttpService
   const mockHttpService = {
     get: jest.fn(),
   };
 
-  // 2. Create a generic mock for Mongoose Models
+  // Mock for Mongoose Models
   const mockModel = {
     find: jest.fn(),
     findOne: jest.fn(),
     findOneAndUpdate: jest.fn(),
-    findByIdAndUpdate: jest.fn(),
     create: jest.fn(),
     exec: jest.fn(),
   };
@@ -27,12 +26,10 @@ describe('PlayersService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PlayersService,
-        // 3. Provide the HttpService mock
         {
           provide: HttpService,
           useValue: mockHttpService,
         },
-        // 4. Provide the Player Model mock
         {
           provide: getModelToken(Player.name),
           useValue: mockModel,
